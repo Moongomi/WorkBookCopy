28fddd4f4aa8462122d39066f8e9b4d5ee2d4560
Moongomi <kyrdudn2@naver.com>
2021-02-09 22:29:15 +0900

    'Alt+2' 액션과 'Alt+3' 액션 추가

--- back.js
+++ back.js
@@ -1,35 +1,82 @@
-var isAlt = false;
-var isOne = false;
-
-var bookName;
+var workBook = [];
 
 function getName(){
-    let test = document.getElementsByClassName('page-header')[0];
-    let t1 = test.getElementsByTagName('span')[0];
-    bookName = t1.outerText;
-    alert(bookName);
+    let arr = [];
+    let bookName = document.getElementsByClassName('page-header')[0].innerText.split('\n');
+    arr.push(bookName);
+    workBook.push(arr);
+}
+
+function getNumbers(){
+    let arr = [];
+    let temp = document.getElementsByClassName('table-striped')[0].getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+    
+    for(var i = 0; i<temp.length; i++){
+        arr.push(Number(temp[i].getElementsByTagName('td')[0].innerText));
+    }
+
+    workBook.push(arr);
+}
+
+function storageData(){
+    chrome.storage.sync.set({'value':workBook},function(){
+        alert('복사 완료');
+    })
+}
+
+function setWorkBook(){
+    chrome.storage.sync.get(['value'],function(result){
+        inputBook(result.value);
+    })
+}
+
+function inputBook(workBook){
+    if(!workBook.length){
+        alert('문제집이 비어있습니다.');
+        return 0;
+    }
+
+    document.getElementsByClassName('form-control')[0].value = workBook[0][0][0];
+    document.getElementsByClassName('form-control')[1].value = workBook[0][0][1];
+    
+   for(var i = 0; i < workBook[1].length; i++){
+        $('input[name=problem]').val(workBook[1][i]);
+        var actualCode = '(' + function(workBook) {
+            var e = jQuery.Event("keypress");
+            e.which = 13;
+            $('input[name=problem]').trigger(e);
+    } + ')();';
+        var script = document.createElement('script');
+        script.textContent = actualCode;
+        (document.head||document.documentElement).appendChild(script);
+        script.remove();
+   }
+}
+
+function delWorkBook(){
+    chrome.storage.sync.remove(['value']);
+    alert('삭제 완료');
 }
 
 function KeyDown(){
-    document.onkeydown = function(event){ //Event listener: once any key is pressed, run this function
-/*
-        if(event.key == "Alt"){ //if Alt is pressed, change the boolean of ctrlpressed to true.
-            isAlt = true;
-        }
-        if(event.key == "1"){
-            isOne = true;
-        }
-        if(isOne == true && isAlt == true){
-            getName();
+    document.onkeydown = function(event){
+       if(event.altKey){
+           {
+               if(event.key=="1"){
+                    getName();
+                    getNumbers();
+                    storageData();
+               }
 
-            isAlt = false;
-            isOne = false;
-        }
-*/
-       if(event.key=="1" && event.altKey){
-           getName();
+               if(event.key == "2"){
+                    setWorkBook();
+               }
+
+               if(event.key == "3"){
+                   delWorkBook();
+               }
+           }
        }
-        
     }
 }
 
